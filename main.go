42535eb7f7afe138157ba761b984cff19a6a066e
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2017-10-02 21:10:53 +0200

    WIP

--- main.go
+++ main.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"io/ioutil"
 	"log"
-	"math/rand"
 	"os"
-	"time"
 
 	"github.com/gonum/matrix/mat64"
 )
-
-type rnnType struct {
-	vocabDimension  int // the size of the vocabulary
-	hiddenDimension int // the size of the hidden layer
-	bpttTruncate    int
-	u               *mat64.Dense // size is hiddenDimension * vocabDimension
-	v               *mat64.Dense // size is vocabDimension * hiddenDimension
-	w               *mat64.Dense // size is hiddenDimension * hiddenDimension
-}
 
 // Implementation of http://www.wildml.com/2015/09/recurrent-neural-networks-tutorial-part-2-implementing-a-language-model-rnn-with-python-numpy-and-theano/
 func main() {
@@ -34,34 +20,11 @@ func main() {
 	//dataSize := len(data)
 	//fmt.Printf("data has %d runes, %d unique.\n", dataSize, vocabDimension)
 
-	rnn := &rnnType{
-		vocabDimension:  len(runesToIx),
-		hiddenDimension: 100,
-	}
-	// Initialize the matrices with random parameters o
-	// Initialize the random seed
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	uData := make([]float64, rnn.vocabDimension*rnn.hiddenDimension)
-	for i := range uData {
-		uData[i] = r.NormFloat64()
-	}
-	rnn.u = mat64.NewDense(rnn.hiddenDimension, rnn.vocabDimension, uData)
-
-	vData := make([]float64, rnn.vocabDimension*rnn.hiddenDimension)
-	r = rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := range vData {
-		vData[i] = r.NormFloat64()
-	}
-	rnn.v = mat64.NewDense(rnn.vocabDimension, rnn.hiddenDimension, vData)
-
-	wData := make([]float64, rnn.hiddenDimension*rnn.hiddenDimension)
-	r = rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := range wData {
-		wData[i] = r.NormFloat64()
-	}
-	rnn.w = mat64.NewDense(rnn.hiddenDimension, rnn.hiddenDimension, wData)
-	log.Println(rnn)
+	// Create a new RNNs
+	// the first argument is the size of the input (the size of the vocabulary)
+	// the second input is the size of the output vector (which is also the size of the vocabulary)
+	// the lase argument is the size of the hidden layer
+	_ = newRNN(len(runesToIx), len(runesToIx), 100)
 
 }
 
@@ -77,25 +40,4 @@ func lossFun(inputs, targets []int, hprev *mat64.Vector) (float64, *mat64.Dense,
 // length is the sample size
 func sample(h *mat64.Vector, seedIx, length int) ([]int, error) {
 	return []int{0}, nil
-}
-
-func getDataAndVocab(input io.Reader) ([]rune, map[rune]int, map[int]rune) {
-	d, err := ioutil.ReadAll(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Extract the rune list
-	runeToIx := make(map[rune]int)
-	data := bytes.Runes(d)
-	for _, v := range data {
-		runeToIx[v] = 0
-	}
-	ixToRune := make(map[int]rune, len(runeToIx))
-	i := 0
-	for k := range runeToIx {
-		runeToIx[k] = i
-		ixToRune[i] = k
-		i++
-	}
-	return data, runeToIx, ixToRune
 }
