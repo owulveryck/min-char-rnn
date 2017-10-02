96587ccba51c1119591b74945ff3b0b634d5f3e2
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2017-10-02 21:12:32 +0200

    Code refactoring

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 // Implementation of http://www.wildml.com/2015/09/recurrent-neural-networks-tutorial-part-2-implementing-a-language-model-rnn-with-python-numpy-and-theano/
 func main() {
 	// Open the sample text file
-	file, err := os.Open("input.txt")
+	file, err := os.Open("data/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
