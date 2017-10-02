ffb598988392f4902d02a9b4c2ef7cf30aff5537
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2017-10-02 20:44:56 +0200

    Wip

---
+++ scratch/helper.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+)
+
+func getDataAndVocab(input io.Reader) ([]rune, map[rune]int, map[int]rune) {
+	d, err := ioutil.ReadAll(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Extract the rune list
+	runeToIx := make(map[rune]int)
+	data := bytes.Runes(d)
+	for _, v := range data {
+		runeToIx[v] = 0
+	}
+	ixToRune := make(map[int]rune, len(runeToIx))
+	i := 0
+	for k := range runeToIx {
+		runeToIx[k] = i
+		ixToRune[i] = k
+		i++
+	}
+	return data, runeToIx, ixToRune
+}
--- scratch/main.go
+++ scratch/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"io/ioutil"
 	"log"
 	"os"
 
@@ -27,7 +24,7 @@ func main() {
 	// the first argument is the size of the input (the size of the vocabulary)
 	// the second input is the size of the output vector (which is also the size of the vocabulary)
 	// the lase argument is the size of the hidden layer
-	rnn := newRNN(len(runesToIx), len(runesToIx), 100)
+	_ = newRNN(len(runesToIx), len(runesToIx), 100)
 
 }
 
@@ -43,25 +40,4 @@ func lossFun(inputs, targets []int, hprev *mat64.Vector) (float64, *mat64.Dense,
 // length is the sample size
 func sample(h *mat64.Vector, seedIx, length int) ([]int, error) {
 	return []int{0}, nil
-}
-
-func getDataAndVocab(input io.Reader) ([]rune, map[rune]int, map[int]rune) {
-	d, err := ioutil.ReadAll(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Extract the rune list
-	runeToIx := make(map[rune]int)
-	data := bytes.Runes(d)
-	for _, v := range data {
-		runeToIx[v] = 0
-	}
-	ixToRune := make(map[int]rune, len(runeToIx))
-	i := 0
-	for k := range runeToIx {
-		runeToIx[k] = i
-		ixToRune[i] = k
-		i++
-	}
-	return data, runeToIx, ixToRune
 }
